93821d7d39859e6456f2e6d44bb6a3c5833bec1a
Lawanyasunkara <111355566+Lawanyasunkara@users.noreply.github.com>
2022-08-22 16:09:20 +0530

    Add files via upload

---
+++ Arrays-22-8/Arrays-22-8/prob4.cs
@@ -0,0 +1,50 @@
+﻿// C# code to remove a range of
+// elements from the ArrayList
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+class prob4
+{
+
+    // Driver code
+    public static void Main()
+    {
+
+        // Creating an ArrayList
+        ArrayList myList = new ArrayList(10);
+
+        // Adding elements to ArrayList
+        myList.Add(2);
+        myList.Add(4);
+        myList.Add(6);
+        myList.Add(8);
+        myList.Add(10);
+        myList.Add(12);
+        myList.Add(14);
+        myList.Add(16);
+        
+
+        // Displaying the elements in ArrayList
+        Console.WriteLine("The initial ArrayList: ");
+
+        foreach (int i in myList)
+        {
+            Console.WriteLine(i);
+        }
+
+        // removing 4 elements starting from index 0
+        myList.RemoveRange(0, 4);
+
+        // Displaying the modified ArrayList
+        Console.WriteLine("The ArrayList after Removing elements: ");
+
+        // Displaying the elements in ArrayList
+        foreach (int i in myList)
+        {
+            Console.WriteLine(i);
+        }
+        
+    }
+}
+
---
+++ Arrays-22-8/Arrays-22-8/prob8.cs
@@ -0,0 +1,40 @@
+﻿// C# program to illustrate how
+// remove elements from the hashtable
+using System;
+using System.Collections;
+
+class prob8
+{
+
+    
+    static public void Main()
+    {
+
+        
+        Hashtable my_hashtable = new Hashtable();
+        my_hashtable.Add("A1", "Welcome");
+        my_hashtable.Add("A2", "to");
+        my_hashtable.Add("A3", "Training");
+
+        // Using remove method
+        // remove A2 key/value pair
+        my_hashtable.Remove("A2");
+
+        Console.WriteLine("Key and Value pairs :");
+
+        foreach (DictionaryEntry ele1 in my_hashtable)
+        {
+            Console.WriteLine("{0} and {1} ", ele1.Key, ele1.Value);
+        }
+
+        // Before using Clear method
+        Console.WriteLine("Total number of elements present" +
+                " in my_hashtable:{0}", my_hashtable.Count);
+
+        my_hashtable.Clear();
+
+        // After using Clear method
+        Console.WriteLine("Total number of elements present in" +
+                    " my_hashtable:{0}", my_hashtable.Count);
+    }
+}
