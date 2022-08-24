4c7a4bf926dfd5e2c280eb1d8b482d86da207644
Lawanyasunkara <111355566+Lawanyasunkara@users.noreply.github.com>
2022-08-24 20:22:41 +0530

    Add files via upload

---
+++ MyApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+namespace MyApp
+{
+    public class Program
+    {
+        public static  void Main(string[] args)
+        {
+            // Prob1.Compare();
+            //Prob2.Copy();
+            // Prob3.Indexof();
+            //Prob6.Makefile();
+            //Prob4.Split();
+            //Prob5.Uppercase();
+            Prob7.Datedetails();
+        }
+    }
+}
