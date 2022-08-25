f5db1dac9a88c3630b1fd1d4b021efe4b8f2eff5
Lawanyasunkara <111355566+Lawanyasunkara@users.noreply.github.com>
2022-08-25 20:23:45 +0530

    Add files via upload

---
+++ 25-8/25-8/prob4.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace prob4
+{
+    internal class Prob4
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("C drive info");
+            DriveInfo info = new DriveInfo("C");
+            Console.WriteLine(info.Name);
+            Console.WriteLine(info.TotalSize);
+            Console.WriteLine(info.DriveType);
+            Console.WriteLine(info.DriveFormat);
+            Console.WriteLine("----------------------------");
+            DriveInfo[] dinfo = DriveInfo.GetDrives();
+            foreach (var item in dinfo)
+            {
+                Console.WriteLine(item.Name);
+            }
+            Console.ReadLine();
+        }
+    }
+}
