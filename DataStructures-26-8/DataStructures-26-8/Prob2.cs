a3c4f517e951cc839f2af5984c21852432a1b345
Lawanyasunkara <111355566+Lawanyasunkara@users.noreply.github.com>
2022-08-28 13:38:34 +0530

    Add files via upload

---
+++ DataStructures-26-8/DataStructures-26-8/Prob2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandsonAug26
+{
+    internal class Prob2
+    {
+        public struct Student
+        {
+
+            // Declare three variables
+            // id, name and age
+            public int Id;
+            public string Name;
+            public int Age;
+
+            // Set the employee details
+            public void SetStudent(int id, string name, int age)
+            {
+                Id = id;
+                Name = name;
+                Age = age;
+            }
+
+            // Display employee details
+            public void DisplayStudent()
+            {
+                Console.WriteLine("Employee:");
+                Console.WriteLine("\tId    : " + Id);
+                Console.WriteLine("\tName   : " + Name);
+                Console.WriteLine("\tAge   : " + Age);
+                Console.WriteLine("\n");
+            }
+
+        }
+    }
+}
---
+++ DataStructures-26-8/DataStructures-26-8/prob1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public struct Student
+{
+    struct student
+    {
+        public int s_id;
+        public String s_name, c_name, s_dob;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            student[] arr = new student[4];
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("Please enter StudentId");
+                    
+                arr[i].s_id = Int32.Parse(Console.ReadLine());
+                Console.WriteLine("Enter StudenName");
+                arr[i].s_name = Console.ReadLine();
+                Console.WriteLine("Enter CourseName");
+                arr[i].c_name = Console.ReadLine();
+                Console.WriteLine("Enter DateOfBirth");
+                arr[i].s_dob = Console.ReadLine();
+            }
+        }
+    }
+
+
+
+}
+
