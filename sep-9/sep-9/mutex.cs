b8f8e150140d34e5d40f356521e0b458e7f902da
Lawanyasunkara <111355566+Lawanyasunkara@users.noreply.github.com>
2022-09-10 20:30:17 +0530

    Add files via upload

---
+++ sep-9/sep-9/mutex.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _9_Sep_Handson
+{
+    class Mutex
+    {
+        static void Main(string[] args)
+        {
+            //imterprocess
+            using (System.Threading.Mutex mutex = new System.Threading.Mutex(false, "ThreadingDemo"))
+            {
+
+                if (!mutex.WaitOne(1000, false))
+                {
+                    Console.WriteLine("already running....");
+                    Console.ReadLine();
+                    return;
+
+                }
+                else
+                {
+                    Console.WriteLine("app is running");
+                    Console.ReadLine();
+                }
+
+            }
+
+        }
+    }
+}
